e493c28c33963b14fba95efc507cc91be401fb65
Thomas Triau <thomas.triau@homeserve.fr>
2019-06-12 13:30:38 +0200

    wip

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-fetch(`https://chat.googleapis.com/v1/spaces/AAAADVFkEvM/messages?key=${process.env.KEY}`, {
+fetch(`https://chat.googleapis.com/v1/spaces/AAAADVFkEvM/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=${process.env.TOKEN}`, {
   method: 'post',
   body:    JSON.stringify({
     text: "TEST webhook 💥."
