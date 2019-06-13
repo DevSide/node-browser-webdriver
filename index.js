c485c1104d04c9968d25230956823609f6d8bead
DevSide <thomas.triau@gmail.com>
2019-06-13 17:50:20 +0200

    chrome + chromedriver (#2)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const fetch = require('node-fetch')
+const { chromeDockerfiles } = require('./chrome')
 
-fetch(`https://chat.googleapis.com/v1/spaces/AAAADVFkEvM/messages?key=${process.env.KEY}`, {
-  method: 'post',
-  body:    JSON.stringify({
-    text: "TEST webhook 💥."
-  }),
-  headers: { 'Content-Type': 'application/json' },
-})
-  .then(res => res.json())
-  .then(json => console.log('post message done'));
+chromeDockerfiles().then(
+  () => {
+    process.exit(0)
+  },
+  error => {
+    console.error(error.message)
+    process.exit(1)
+  }
+)
